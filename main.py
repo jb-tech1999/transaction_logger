8173092d29f540338c7aa60aa3b34c02100b5d34
jb-tech1999 <65624057+jb-tech1999@users.noreply.github.com>
2020-05-21 15:13:11 +0200

    Can now add user
    User can now be added

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ def  login():
     print("\t|            Rapoeli v0.1                    |")
     print("\t==============================================")
     print("\t| 1. Login                                   |")
-    print("\t| 2. Exit                                    |")
+    print("\t| 2. Add user                                |")
+    print("\t| 3. Exit                                    |")
     print("\t==============================================")
 
 
@@ -20,13 +21,11 @@ def  login():
 def main_menu():
     os.system("cls")
     print("\t==============================================")
-    print("\t|            Rapoeli v0.1                    |")
+    print("\t|            Rapoeli v0.3                    |")
     print("\t==============================================")
     print("\t| 1. Enter Transaction                       |")
     print("\t| 2. Show transactions                       |")
     print("\t| 3. Search                                  |")
-    print("\t==============================================")
-    print("\t| 4. Add users                               |")
     print("\t==============================================")
     print("\t| x. Exit                                    |")
     print("\t==============================================")
@@ -41,34 +40,65 @@ def check_login(username):
      query = "SELECT * FROM users WHERE userName = "
      found = True
      while found:
-         username = '''username'''
+         username = "'" + username + "'"
          tmp_query = query+username
-         print(tmp_query)
          cursor.execute(tmp_query)
          user_data = cursor.fetchone()
          if user_data != "":
              found = False
              return user_data
+         else:
+             break
+
+def add_user(username, password):
+    cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
+                          "Server=DESKTOP-PND5I06\SQLEXPRESS;"
+                          "Database=rapoeli;"
+                          "Trusted_Connection=yes;")
+    cursor = cnxn.cursor()
+    query = ("INSERT INTO users VALUES(?, ?)")
+    cursor.execute(query, [username, password])
+    cnxn.commit()
 
 
 
 
-
-
-login()
-choice = int(input("Enter choice: "))
-if choice == 1:
-    username = enterbox("Username:")
+login() # print the login menu
+choice = int(input("Enter choice: ")) # asks for a choice
+if choice == 1: # if choice = 1, user wil be prompt to enter username, if username is found, check for password
+    username = enterbox("Enter username: ", "Username")
     print(check_login(username))
-    i = 0
-    while i < 3:
-        password = passwordbox("Password:")
-        if password in check_login(username):
+    if check_login(username) != "None":
+        run = False
+        i = 0
+        while i < 3:
+            password = passwordbox("Enter password","Password")
+            if password in check_login(username):
+                main_menu()
+                ask = int(input("Enter choice"))
+
+            else:
+                print("Please try again")
+                i += 1
+    else:
+        login()
+
+elif choice == 2:
+    run = True
+    username = enterbox("Enter username", "Username ")
+    while run:
+        password = passwordbox("Enter password ", "Password")
+        password2 = passwordbox("Enter password again", "Password check")
+        if password == password2:
+            add_user(username, password)
+            run = False
             main_menu()
-            break
+            ask = int(input("Enter choice"))
         else:
-            print("Please try again")
-            i += 1
+            print("Password doesn't match")
+
+
+
 
 
 else:
